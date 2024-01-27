1aa8164f693e28ec95b7326658c6f861f10ffec7
aulin1 <audrey.youran.lin@gmail.com>
2024-01-26 21:10:06 -0500

    finished title screen with placeholders

--- Assets/Scripts/Title_Script.cs
+++ Assets/Scripts/Title_Script.cs
@@ -1,22 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Title_Script : MonoBehaviour
 {
-    public AudioSource audio;
-    
+    [SerializeField]
+    private AudioSource audio;
+    [SerializeField]
+    private GameObject background_1;
+    [SerializeField]
+    private GameObject background_2;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        //set background
+        background_1.SetActive(true);
+        background_2.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         if(!audio.isPlaying){
-            //switch the two, activate the buttons etc
+            //switch the two
+            background_1.SetActive(false);
+            background_2.SetActive(true);
         }
+    }
+
+    //on click for start button
+    public void start_game(){
+        SceneManager.LoadScene("SampleScene");
     }
 }
